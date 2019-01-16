b0a16150b3bf9eabddf1d9611b145438e72e7ea5
Александр <CHUGGUN@outlook.com>
2019-01-16 13:31:04 +0600

    started emulation

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace Test
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            
+            var conf = Configuration();
+            if (conf == null) return;
+
+
+
+            Console.WriteLine(conf.GetValue<string>("ApiHost"));
+
+            Console.ReadLine();
+        }
+
+        private static IConfigurationRoot Configuration()
+        {
+            try
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+
+                return builder.Build();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
         }
     }
 }

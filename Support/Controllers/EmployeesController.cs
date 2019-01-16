0c7229016a06ddd410e71a5ebde1b393cb1eacc7
Александр <CHUGGUN@outlook.com>
2019-01-16 23:23:13 +0600

    initial test console output

---
+++ Support/Controllers/EmployeesController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Support.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly SupportContext _context;
+
+        public EmployeesController(SupportContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return _context.Employees;
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        // PUT: api/Employees/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee([FromRoute] string id, [FromBody] Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.Login)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        [HttpPost]
+        public async Task<IActionResult> PostEmployee([FromBody] Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.Login }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        private bool EmployeeExists(string id)
+        {
+            return _context.Employees.Any(e => e.Login == id);
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -6,17 +6,39 @@ namespace Test
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             var conf = Configuration();
-            if (conf == null) return;
 
+            if (conf == null) goto Fin;
 
+            if (args.Length > 0)
+                switch (args[0].Trim('-').ToLower())
+                {
+                    case "emulate":
+                        break;
+                    case "info":
+                        break;
+                    case "clear":
+                        break;
+                    default:
+                        ArgumentsError();
+                        return;
+                }
+            else ArgumentsError();
 
-            Console.WriteLine(conf.GetValue<string>("ApiHost"));
-
+            Fin:
+            Console.WriteLine("Press Enter to exit..");
             Console.ReadLine();
+        }
+
+        private static void ArgumentsError()
+        {
+            Console.WriteLine("Check arguments!");
+            Console.WriteLine("Valid options are: emulate, info, clear.");
+            Console.WriteLine("  Emulate - perform emulation;");
+            Console.WriteLine("  Info - show emulation parameters, employees and messages queue statistics;");
+            Console.WriteLine("  Clear - clear messages queue.");
         }
 
         private static IConfigurationRoot Configuration()
